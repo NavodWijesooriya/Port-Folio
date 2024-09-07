aabf4d45f2f651864141392557f98a47cc6001de
NavodWijesooriya <navodtwijesooriya@gmail.com>
2024-09-07 11:25:09 +0530

    create hero section

--- Port-Folio/src/App.jsx
+++ Port-Folio/src/App.jsx
@@ -1,5 +1,7 @@
 import styles from './App.module.css'
 import { NavBar } from './components/NavBar';
+import { Hero } from './components/Hero';
+
 
 function App() {
  
@@ -7,6 +9,7 @@ function App() {
   return (
     <div className={styles.App}>
    <NavBar />
+   <Hero />
     </div>
   )
 }
