bb70ae3b436179770df6cdb58f09b88f44fb6cfa
NavodWijesooriya <navodtwijesooriya@gmail.com>
2024-09-11 00:00:55 +0530

    changr email

--- Port-Folio/src/components/Contact.jsx
+++ Port-Folio/src/components/Contact.jsx
@@ -13,7 +13,7 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="navodtwijesooriya@gmail.com">navodtwijesooriya@gmail.com</a>
+          <a href="mailto:navodtwijesozoriya@gmail.com">navodtwijesooriya@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
